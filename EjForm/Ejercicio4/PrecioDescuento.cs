92c65475b1410eb7ad0f66a527cbe19a469bba19
diegovilca <diegovilca@gmail.com>
2016-09-03 02:44:40 -0300

    Intentando crear objeto ComboBox fuera de la clase Form.

--- EjForm/Ejercicio4/PrecioDescuento.cs
+++ EjForm/Ejercicio4/PrecioDescuento.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Ejercicio4
 {
     public class PrecioDescuento
     {
-        double precioLampara = 35;
-        double precioFinal;
-        
+        static double precio = 35;
+        static double precioFinal;
+        ComboBox marca;
+
         public static double CalcularPrecio(int cantidad)
-        { 
-              
+        {
+
+            if (cantidad >= 6)
+            { 
+                precioFinal = precio
+            }
+
+
         }//Fin CalcularPrecio(int)
 
     }
